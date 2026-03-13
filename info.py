81b4eace0d72a849ac397de0df201dcf8aa212c4
dumpm499-cmd <dumpm499@gmail.com>
2026-03-13 22:41:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ from Script import script
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION_STRING = environ.get('SESSION_STRING', '')
 API_ID = int(environ.get('API_ID', '0'))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
